8be561b6cbf87394271e56767de4ccd4ef041b43
Bruno Carvalhal <hello@brunocarvalhal.com>
2023-03-25 13:43:55 +0100

    Implement day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,18 @@
-pub fn part1(_data: &str) {
-    println!("Hello, World 1");
+pub fn part1(data: &str) {
+    let max: u64 = data
+        .split("\n\n")
+        .map(|group| {
+            group
+                .split("\n")
+                .map(|item| item.parse::<u64>().expect("Invalid amount"))
+                .sum()
+        })
+        .max()
+        .expect("No groups found");
+
+    println!("Result: {max}");
 }
 
 pub fn part2(_data: &str) {
-    println!("Hello, World 2");
+    unimplemented!();
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ pub mod day1;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() < 3 {
+
+    if args.len() < 2 {
         panic!("No day provided");
+    }
+
+    if args.len() < 3 {
+        panic!("No part provided");
     }
 
     let day: u8 = match args[1].trim().parse() {
