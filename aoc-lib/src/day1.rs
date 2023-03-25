b71bd2dcfd1a107f284371369fdd9152fa03047d
Bruno Carvalhal <hello@brunocarvalhal.com>
2023-03-25 14:54:24 +0100

    Decrease day1 runtime by -17% by collecting early

--- aoc-lib/src/day1.rs
+++ aoc-lib/src/day1.rs
@@ -2,21 +2,25 @@ use itertools::Itertools;
 
 pub fn part1(data: &str) -> String {
     return parse_elves(data)
+        .iter()
         .max()
         .expect("No groups found")
         .to_string();
 }
 
 pub fn part2(data: &str) -> String {
-    let result: u64 = parse_elves(data).sorted().rev().take(3).sum();
+    let result: u64 = parse_elves(data).iter().sorted().rev().take(3).sum();
     return result.to_string();
 }
 
-fn parse_elves(data: &str) -> impl Iterator<Item = u64> + '_ {
-    return data.split("\n\n").map(|group| {
-        group
-            .split("\n")
-            .map(|item| item.parse::<u64>().expect("Invalid amount"))
-            .sum()
-    });
+fn parse_elves(data: &str) -> Vec<u64> {
+    return data
+        .split("\n\n")
+        .map(|group| {
+            group
+                .split("\n")
+                .map(|item| item.parse::<u64>().expect("Invalid amount"))
+                .sum()
+        })
+        .collect();
 }
