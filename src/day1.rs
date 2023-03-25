2e433b137dbde09e3151a07e1e83896f86fb50e7
Bruno Carvalhal <hello@brunocarvalhal.com>
2023-03-25 13:28:25 +0100

    Implement runner

---
+++ src/day1.rs
@@ -0,0 +1,7 @@
+pub fn part1(_data: &str) {
+    println!("Hello, World 1");
+}
+
+pub fn part2(_data: &str) {
+    println!("Hello, World 2");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::fs;
+
+pub mod day1;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 3 {
+        panic!("No day provided");
+    }
+
+    let day: u8 = match args[1].trim().parse() {
+        Ok(day) => day,
+        Err(_) => panic!("Invalid day provided"),
+    };
+
+    let part: u8 = match args[2].trim().parse() {
+        Ok(part) => part,
+        Err(_) => panic!("Invalid part provided"),
+    };
+
+    let path = format!("data/{}.in", args[1]);
+    let data = fs::read_to_string(path).expect("No data found for this day");
+    execute(day, part, &data);
+}
+
+// TODO: make Day trait
+fn execute(day: u8, part: u8, data: &str) {
+    match day {
+        1 => {
+            if part == 1 {
+                day1::part1(data)
+            } else if part == 2 {
+                day1::part2(data)
+            } else {
+                panic!("Part does not exist")
+            }
+        }
+        _ => panic!("Solution for day {day}, part {part} not implemented yet."),
+    }
 }
