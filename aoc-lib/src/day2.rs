911c6de483fd9699edac11b0d56281b259e401d8
Bruno Carvalhal <hello@brunocarvalhal.com>
2023-03-26 15:13:01 +0200

    Optimize day2

--- aoc-lib/src/day2.rs
+++ aoc-lib/src/day2.rs
@@ -1,42 +1,27 @@
 pub fn part1(data: &str) -> String {
     let lines = data.split("\n");
-    let sum: u32 = lines.map(get_score).sum();
+    let sum: u32 = lines.map(|line| get_score(line, true)).sum();
 
     return sum.to_string();
 }
 
 pub fn part2(data: &str) -> String {
     let lines = data.split("\n");
-    let sum: u32 = lines.map(get_score_2).sum();
+    let sum: u32 = lines.map(|line| get_score(line, false)).sum();
     return sum.to_string();
 }
 
-fn get_score(line: &str) -> u32 {
-    match line {
-        "A X" => 4,
-        "A Y" => 8,
-        "A Z" => 3,
-        "B X" => 1,
-        "B Y" => 5,
-        "B Z" => 9,
-        "C X" => 7,
-        "C Y" => 2,
-        "C Z" => 6,
-        _ => 0,
-    }
-}
+fn get_score(line: &str, part1: bool) -> u32 {
+    let scores = if part1 {
+        [4, 8, 3, 1, 5, 9, 7, 2, 6]
+    } else {
+        [3, 4, 8, 1, 5, 9, 2, 6, 7]
+    };
 
-fn get_score_2(line: &str) -> u32 {
-    match line {
-        "A X" => 3,
-        "A Y" => 4,
-        "A Z" => 8,
-        "B X" => 1,
-        "B Y" => 5,
-        "B Z" => 9,
-        "C X" => 2,
-        "C Y" => 6,
-        "C Z" => 7,
-        _ => 0,
-    }
+    let first = line.chars().next().unwrap();
+    let last = line.chars().last().unwrap();
+
+    let first = first as u8 - 'A' as u8;
+    let last = last as u8 - 'X' as u8;
+    return scores[(first * 3 + last) as usize];
 }
